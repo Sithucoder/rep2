2fc3afff27b55ad4779cde6980063946c80158d3
Sithartha S G <Sithartha S G@DESKTOP-SVPGF28>
2023-06-24 08:55:01 +0530

    helo

---
+++ sub/src/helo/ind.java
@@ -0,0 +1,15 @@
+package helo;
+
+public class ind {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,c;
+		a=25;
+		b=10;
+		c=a-b;
+		System.out.println(c);
+
+	}
+
+}
